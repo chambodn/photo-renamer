f89a4bdf05432660a1dd64efc8d23b4175a0a43b
chambodn <vidou@iMac-de-Vidou.local>
2018-06-10 21:08:32 +0200

    Initialize project with dedicated branch

--- main/main.go
+++ main/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"time"
 
+	"go.uber.org/zap"
+
+	"github.com/chambodn/photo-renamer/log"
 	"github.com/xiam/exif"
 )
 
@@ -13,9 +19,29 @@ func main() {
 	}
 	//Date and Time = 2016:10:31 06:55:35
 	//only interested in Date and time at this stage:
-	fmt.Printf("Youhou = %s\n\n", data.Tags["Date and Time"])
+	dateAndTime := data.Tags["Date and Time"]
+	log.Logger.Info("Extract Metadata", zap.String("DateTime", dateAndTime))
 
+	value := strings.Split(dateAndTime, " ")
+	log.Logger.Info("Split Time and Date", zap.String("Date", value[0]), zap.String("Time", value[1]))
+
+	sDate := strings.Replace(value[0], ":", "-", -1)
+	log.Logger.Info("We need to replace ':' by '-", zap.String("Date", sDate))
+
+	if _, err := os.Stat(sDate); os.IsNotExist(err) {
+		// directory sDate does not exist
+		log.Logger.Warn("Directory does not exit", zap.String("Directory", sDate))
+	}
+
+	if _, err := os.Stat("/path/to/whatever"); err == nil {
+		log.Logger.Warn("Directory exists", zap.String("Directory", sDate))
+	}
 	//layout := "2016:10:31 06:55:35"
 
 }
-,
+
+func parseDate(date string) time.Time {
+	log.Logger.Info("salut")
+
+	return time.Now()
+}
